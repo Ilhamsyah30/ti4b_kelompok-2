5ac029ae2a873e6879957d77ee03303468188e56
reza <mrezan9f@gmail.com>
2024-07-16 19:37:58 +0700

    add form add user

---
+++ page/user/add.php
@@ -0,0 +1,89 @@
+<?php 
+if(isset($_POST['add'])) {
+	$NIM = htmlspecialchars($_POST['NIM']);
+	$FullName = htmlspecialchars($_POST['FullName']);
+	$DateOfBirth = htmlspecialchars($_POST['DateOfBirth']);
+	$Gender = htmlspecialchars($_POST['Gender']);
+	$Email = htmlspecialchars($_POST['Email']);
+	$PhoneNumber = htmlspecialchars($_POST['PhoneNumber']);
+	$Address = htmlspecialchars($_POST['Address']);
+	$JoinDate = htmlspecialchars($_POST['JoinDate']);
+	$MembershipStatus = htmlspecialchars($_POST['MembershipStatus']);
+	$YearOfStudy = htmlspecialchars($_POST['YearOfStudy']);
+	$StudyProgram = htmlspecialchars($_POST['StudyProgram']);
+
+	$sql = $conn->query("INSERT INTO tbl_member VALUES (null, '$NIM', '$FullName', '$DateOfBirth', '$Gender', '$Email', '$PhoneNumber', '$Address', '$JoinDate', '$MembershipStatus', '$YearOfStudy', '$StudyProgram')") or die(mysqli_error($conn));
+	if($sql) {
+		echo "<script>alert('Success.');window.location='?p=user';</script>";
+	} else {
+		echo "<script>alert('Failse.')</script>";
+	}
+}
+
+?>
+<div class="container">
+  <nav class="mt-4" style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='currentColor'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+      <li class="breadcrumb-item"><a href="index.php?p=user">Member</a></li>
+      <li class="breadcrumb-item active" aria-current="page">Add Member</li>
+    </ol>
+  </nav>
+  <div class="card mb-4">
+      <div class="card-header">
+        <i class="bi bi-plus mr-1"></i>
+        Add Member
+      </div>
+      <div class="card-body">
+        <form action="" method="post">
+          <div class="mb-3">
+            <label for="NIM" class="form-label">NIM</label>
+            <input type="text" class="form-control" id="NIM" name="NIM" placeholder="ex ...">
+          </div>
+          <div class="mb-3">
+            <label for="FullName" class="form-label">Full Name</label>
+            <input type="text" class="form-control" id="FullName" name="FullName" placeholder="ex ..." required>
+          </div>
+          <div class="mb-3">
+            <label for="DateOfBirth" class="form-label">Date of Birth</label>
+            <input type="text" class="form-control" id="DateOfBirth" name="DateOfBirth" placeholder="ex ..." required>
+          </div>
+          <div class="mb-3">
+            <label for="Gender" class="form-label">Gender</label>
+            <select name="Gender" id="Gender" class="form-control" required>
+              <option value="">-- Choise Gender --</option>
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+            </select>
+          </div>
+          <div class="mb-3">
+            <label for="Email" class="form-label">Email</label>
+            <input type="email" class="form-control" id="Email" name="Email" placeholder="ex ..." required>
+          </div>
+          <div class="mb-3">
+            <label for="PhoneNumber" class="form-label">Phone Number</label>
+            <input type="number" class="form-control" id="PhoneNumber" name="PhoneNumber" placeholder="ex ..." required>
+          </div>
+          <div class="mb-3">
+            <label for="Address" class="form-label">Address</label>
+            <input type="text" class="form-control" id="Address" name="Address" placeholder="ex ..." required>
+          </div>
+          <div class="mb-3">
+            <label for="JoinDate" class="form-label">JoinDate</label>
+            <input type="text" class="form-control" id="JoinDate" name="JoinDate" placeholder="ex ..." required>
+          </div>
+          <div class="mb-3">
+            <label for="MembershipStatus" class="form-label">Membership Status</label>
+            <select name="MembershipStatus" id="MembershipStatus" class="form-control" required>
+              <option value="">-- Choise Membership Status --</option>
+              <option value="Active">Active</option>
+              <option value="Inactive">Inactive</option>
+            </select>
+          </div>
+          <div class="form-group">
+            <button type="submit" class="btn btn-primary" name="tambah">ADD</button>
+          </div>
+        </form>
+      </div>
+  </div>
+</div>
