30bb33e91ae6ff6473f70582d79058dc3698d938
reza <mrezan9f@gmail.com>
2024-07-19 15:06:37 +0700

    breadcrumb returned

--- page/transaction/return.php
+++ page/transaction/return.php
@@ -11,6 +11,7 @@ $getTransactions = $conn->query("SELECT * FROM tbl_transaction INNER JOIN tbl_me
   <nav class="mt-4" style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='currentColor'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
     <ol class="breadcrumb">
       <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+      <li class="breadcrumb-item"><a href="index.php?p=transaction">Transaction</a></li>
       <li class="breadcrumb-item active" aria-current="page">List Transaction Returned</li>
     </ol>
   </nav>
